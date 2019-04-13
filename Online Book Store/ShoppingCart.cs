e65923ff6b797ea3b24a5d82054577361c49794e
tarikkantar <tarikkantar25@gmail.com>
2019-04-13 17:29:12 +0300

    ShoppingCart.cs oluşturdum

---
+++ Online Book Store/ShoppingCart.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Book_Store 
+{
+    class ShoppingCart
+    {
+        public Customer customerID;
+        private List<ItemToPurchase> itemsToPurchase;
+        private double paymentAmount;
+        enum paymentType { cash, creditcard }
+
+        public ShoppingCart()
+        {
+            itemsToPurchase = new List<ItemToPurchase>();
+        }
+        public void printProducts()
+        {
+
+        }
+        public void addProduct(ItemToPurchase item)
+        {
+
+        }
+
+        public void removeProduct(ItemToPurchase item)
+        {
+
+        }
+
+        public void placeOrder()
+        {
+
+        }
+
+        public void cancelOrder()
+        {
+
+        }
+        public void sendInvoidcebyEmail()
+        {
+
+        }
+    }
+}
