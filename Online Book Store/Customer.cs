6bfa51437c1b904729f706681ad89afe14f91dfa
tarikkantar <tarikkantar25@gmail.com>
2019-04-13 16:53:33 +0300

    Customer.cs oluşturdum

---
+++ Online Book Store/Customer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication12
+{
+    class Customer
+    {
+        private string customerID;
+        private string name;
+        private string adress;
+        private string email;
+        private string username;
+        private string password;
+
+        public string CustomerID
+        {
+            get
+            {
+                return customerID;
+            }
+
+            set
+            {
+                customerID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Adress
+        {
+            get
+            {
+                return adress;
+            }
+
+            set
+            {
+                adress = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+        private void printCustomerDetails()
+        {
+            
+        }
+        private void saveCustomer()
+        {
+
+        }
+        private void printCustomerPurchases()
+        {
+
+        }
+
+    }
+
+   
+}
