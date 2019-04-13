0f37d0d1b61082a130601b90bc17a182c6297af2
berkantcnrgl <berkant9873@gmail.com>
2019-04-13 16:27:07 +0300

    Proje oluşturuldu.

---
+++ Online Book Store/Magazine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Book_Store
+{
+    class Magazine : Product
+    {
+        private string issue;
+
+        public string Issue { get => issue; set => issue = value; }
+
+        enum Type {Actual, News, Sport, Computer,Technology}
+
+
+    }
+}
