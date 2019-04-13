28dffd14bef0faaaea1963f245fdad4d3bb4e9cb
tarikkantar <tarikkantar25@gmail.com>
2019-04-13 17:28:26 +0300

    ItemToPurchase.cs oluşturdum

---
+++ Online Book Store/ItemToPurchase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Book_Store
+{
+    class ItemToPurchase
+    {
+        private string product;
+        private string quantity;
+
+        public string Product
+        {
+            get
+            {
+                return product;
+            }
+
+            set
+            {
+                product = value;
+            }
+        }
+
+        public string Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+            }
+        }
+    }
+}
