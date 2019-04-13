7fc3902b5fa69ad44f77edf9dffc7c96bd783213
tarikkantar <tarikkantar25@gmail.com>
2019-04-13 16:35:30 +0300

    Book.cs oluşturdum

--- Online Book Store/Book.cs
+++ Online Book Store/Book.cs
@@ -8,5 +8,75 @@ namespace WindowsFormsApplication12
 {
     class Book
     {
+        private string isbn;
+        private string author;
+        private string publisher;
+        private int page;
+        private string picture;
+
+        public string Isbn 
+       {
+            get
+            {
+                return isbn;
+            }
+
+            set
+            {
+                isbn = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return publisher;
+            }
+
+            set
+            {
+                publisher = value;
+            }
+        }
+
+        public int Page
+        {
+            get
+            {
+                return page;
+            }
+
+            set
+            {
+                page = value;
+            }
+        }
+
+        public string Picture
+        {
+            get
+            {
+                return picture;
+            }
+
+            set
+            {
+                picture = value;
+            }
+        }
     }
 }
